d801fe497b4109ee00aeb3722c9731a4211ab2a6
Si7C-08 <Si7C-08@Si7A-30>
2025-08-21 11:51:25 +0900

    작업중

--- dbListTest2/src/main/java/dbListTest2/servlet/ForwardServlet.java
+++ dbListTest2/src/main/java/dbListTest2/servlet/ForwardServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -12,8 +14,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dbListTest2.process.NotFound;
+import dbListTest2.process.Process;
+import dbListTest2.process.bbs.*;
+
 public class ForwardServlet extends HttpServlet{
-	Connection conn;
+	private Connection conn;
+	private String prefix = "/WEB-INF/views";
+	private String suffix = ".jsp";
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
@@ -44,8 +52,11 @@ public class ForwardServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 유저가 웹에서 접속한 경로를 contextPath만 뺀채로 uri변수에 담는다
+		String uri = request.getRequestURI();
 		String contextPath = request.getContextPath();
-		String uri = request.getRequestURI().substring(contextPath.length());
+		
+		// 유저가 접속시도한 링크의 뒷부분으로 만드는곳
+		String cmd = uri.substring(contextPath.length());
 		// 요청으로 받은값이 읽는것만 하는 get 인가
 		// 아니면 서버에 직접적으로 쓰는것을 원하는 post 인가 를 확인하는
 		// 메서드(함수아님)를 변수에 담아둔다
@@ -54,7 +65,28 @@ public class ForwardServlet extends HttpServlet{
 		
 		// 다형성을 활용해서 다시 만들어보자
 		
+		Map<String, Process> cmd_mapper = new HashMap<>();
 		
+		cmd_mapper.put("/bbs/list", new BBS_List());
+		cmd_mapper.put("/bbs/add", new BBS_Add());
+		cmd_mapper.put("/bbs/edit", new BBS_Edit());
+		cmd_mapper.put("/bbs/delete", new BBS_Delete());
+		cmd_mapper.put("/404", new NotFound());
+		
+		Process service = cmd_mapper.get(cmd);
+		
+		if(service != null) {
+			String nextServlet = service.process(request, response);
+			
+			if(nextServlet.endsWith("redirect:")) {
+				response.sendRedirect(nextServlet.replace("redirect:", contextPath));
+			} else {
+				request.getRequestDispatcher(prefix + nextServlet + suffix).forward(request, response);				
+			}
+		} else {
+			// 잘못된 페이지 링크로 들어왔을때 404로 리다이렉트 시켜버림
+			response.sendRedirect(contextPath + "/404"); 
+		}
 		
 		// uri에 담긴 변수에 따라 분기분(if)으로 어디로 가는지 정해준다
 		
@@ -62,7 +94,7 @@ public class ForwardServlet extends HttpServlet{
 		if(uri.equals("/404")) {
 		// 페이지를 잘못 찾아왔을때 오는곳
 		// index.jsp로 온다
-			request.getRequestDispatcher("/WEB-INF/404.jsp").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/views/404.jsp").forward(request, response);
 		} else if(uri.equals("/bbs/list")) {
 		// bbs 테이블의 목록을 보여주는 페이지
 			// db에서 테이블 목록을 불러와서 list에 넣는다
@@ -72,13 +104,13 @@ public class ForwardServlet extends HttpServlet{
 			// 저장해둔 list를 request객체의 attribute에 담아둔다
 			request.setAttribute("bbs_list", list);
 			// 담아둔 정보와 함께 list.jsp페이지로 포워드 한다
-			request.getRequestDispatcher("/WEB-INF/bbs/bbs_list.jsp").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/views/bbs/bbs_list.jsp").forward(request, response);
 		} else if(uri.equals("/bbs/add")) {
 		// bbs 테이블에 내용(컬럼)을 추가해주는 페이지
 			if(method.equals("GET")) {
 				// 이 페이지를 링크를 통해 혹은 직접들어왔을경우
 				// 테이블을 추가해주는 bbs_add.jsp로 포워드해준다
-				request.getRequestDispatcher("/WEB-INF/bbs/bbs_add.jsp").forward(request, response);				
+				request.getRequestDispatcher("/WEB-INF/views/bbs/bbs_add.jsp").forward(request, response);				
 			} else if(method.equals("POST")) {
 				// 이 페이지로 POST방식의 요청이 들어왔을때
 				// 전달 받은 데이터로 DB에 컬럼을 추가시킨다
@@ -89,14 +121,14 @@ public class ForwardServlet extends HttpServlet{
 		} else if(uri.equals("/bbs/edit")) {
 		// bbs 테이블 목록에서 선택된 컬럼의 id로부터 올수있는 페이지
 		// 테이블 내용을 수정할 수 있다
-			request.getRequestDispatcher("/WEB-INF/bbs/bbs_edit.jsp").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/views/bbs/bbs_edit.jsp").forward(request, response);
 		} else if(uri.equals("/bbs/delete")) {
 		// bbs 테이블의 내용을 삭제 해주는곳
 		// 여기서 특정 테이블의 컬럼을 삭제할 수 있다
 		// 별도의 페이지가 있지는 않다
 			
 		// 삭제가 끝나면 리스트 페이지로 돌아옴
-			request.getRequestDispatcher("/WEB-INF/bbs/bbs_list.jsp").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/views/bbs/bbs_list.jsp").forward(request, response);
 		} else {
 			// 지정되지 않은 경로로 이동시에 index링크로 리다이렉트시킨다
 			response.sendRedirect(contextPath + "/404");
